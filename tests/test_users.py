ad0d24856389c20bdfa0779c9063c926a017ec67
sandrii <andriyshvorak@gmail.com>
2022-02-08 23:57:54 +0200

    Test user creation added

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,26 @@
 from fastapi.testclient import TestClient
 from fastapi_example.main import app
 from fastapi_example import schemas
+from fastapi_example.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+ 
 
 client = TestClient(app)
+engine = create_engine(f"postgresql://{settings.db_username}:"
+                       f"{settings.db_password}@{settings.db_address}:"
+                       f"{settings.db_port}/{settings.db_name}")
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 def test_root():
